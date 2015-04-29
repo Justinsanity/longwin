969422558d039e9f10d92554bbf4995c931971ad
fbukevin <fbukevin@gmail.com>
2015-04-29 18:28:56 +0800

    init

---
+++ css/statuses_sample.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('display_errors', 1);
+require_once('TwitterAPIExchange.php');
+
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => '1274191327-IPRjFc8mijSO2bfXHFuJm4lyD03lpRCQLTLXnlz',
+    'oauth_access_token_secret' => 'KIEj9JmHjx2A5yT5NEjWjEcFA621UvZuHYJrWqYGSUQ',
+    'consumer_key' => 'U9VQ74TGNcs2A0GCzWefg',
+    'consumer_secret' => 'WSrkrvIDtpOHXYzh9OI0DNRFsSxyLWJieKoF8NgCKj0'
+);
+
+/** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
+$url = 'https://stream.twitter.com/1.1/statuses/sample.json';
+$requestMethod = 'GET';
+$getfields = '';
+
+
+/** Perform a POST request and echo the response **/
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->buildOauth($url, $requestMethod)
+             ->setGetfield($getfields)
+             ->performRequest();
+
+
+?>
